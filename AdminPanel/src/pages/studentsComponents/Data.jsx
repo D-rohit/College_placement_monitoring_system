92db524e6c1bfe573637d51ed69765ef8bd938eb
sDevarsh <shingaladevarsh1@gmail.com>
2024-09-28 13:10:54 +0530

    student details from database completed

--- AdminPanel/src/pages/studentsComponents/Data.jsx
+++ AdminPanel/src/pages/studentsComponents/Data.jsx
@@ -1,14 +1,7 @@
-import axios from "axios";
-const token = localStorage.getItem("token");
+// import axios from "axios";
+// const token = localStorage.getItem("token");
 
-const response = await axios.get(
-  "http://localhost:3000/api/student/getAllStudents",
-  {
-    headers: {
-      Authorization: `Bearer ${token}`, // Passing the token in the Authorization header
-    },
-  }
-);
-const studentData =response.data
 
-export default studentData;
+// const studentData =response.data
+
+// export default studentData;
