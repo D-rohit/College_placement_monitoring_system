016a136a9bfc0b881b9a30aafcc2c045cf4f0915
D-rohit <dhruv.37@outlook.com>
2024-11-08 13:12:13 +0530

    Content.jsx updated: for better UI

--- AdminPanel/src/layout/Content.jsx
+++ AdminPanel/src/layout/Content.jsx
@@ -5,12 +5,23 @@ import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 const Content = ({ open, children }) => {
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+
+  const opendWidth = `${viewportWidth - 250}px`; // content width when sidebar is open
+  const closedWidth = `${viewportWidth - 80}px`; // content width when sidebar is closed
+
+  const contentHeight = `${viewportHeight - 100}px`; // content height  
+
+
   return (
     <Box
       component="main"
       sx={{
         flexGrow: 1,
         padding: 3,
+        width:{ sm: open ? opendWidth : closedWidth},
+        height: contentHeight,
         marginLeft: { sm: open ? '240px' : '60px' }, // Responsive margin for different screen sizes
         transition: 'margin-left 0.1s ease-in-out',
         marginTop: '64px', // Adjust for AppBar/header height
